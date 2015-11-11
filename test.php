9e3c45f36a8cddcc781836dd8688e47e62eb3543
amestica5 <martin.amestica@gmail.com>
2015-11-11 19:51:10 -0300

    Update test.php

--- test.php
+++ test.php
@@ -12,9 +12,9 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully";
 
-$consulta = mysql_query('SELECT * 
+$consulta = mysql_query("SELECT * 
 FROM  `test` 
-WHERE id <1500050');
+WHERE id <1500050");
 var_dump($consulta);
 for($i=0;$i<50;$i++){
 echo $consulta[$i];
