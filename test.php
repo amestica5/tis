95682e02e37f6dc2d494239d72f607947ec944a1
amestica5 <martin.amestica@gmail.com>
2015-11-11 20:19:03 -0300

    Update test.php

--- test.php
+++ test.php
@@ -10,7 +10,11 @@ $conn = new mysqli($servername, $username, $password,$basedatos);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+} else if ($result = $conn->query("SELECT COUNT(*) FROM  `test` WHERE id < 1500050")) {
+    while($obj = $result->fetch_asoc()){
+        echo $result;   
+    }
+}
 echo "Connected successfully";
 
 $resultado= mysql_query("SELECT COUNT(*) FROM  `test` WHERE id < 1500050");
