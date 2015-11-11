7b90fd3c564e7ac8bbe6f26abf74c06a5d1be7a6
amestica5 <martin.amestica@gmail.com>
2015-11-11 19:26:51 -0300

    Update test.php

--- test.php
+++ test.php
@@ -3,8 +3,12 @@ $servername = $_SERVER["host"];
 $username = $_SERVER["user"];
 $password = $_SERVER["pass"];
 
+echo $servername;
+echo $username;
+echo $password;
+
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+//$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
