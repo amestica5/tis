629c43cf1bedc99b67f8c8fb19b8b9628e473531
amestica5 <martin.amestica@gmail.com>
2015-11-11 18:29:43 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,5 @@
 <?php
-$mysql_host="54.68.135.137";
-$mysql_user="user"; 
-$mysql_password="pepito.P0";
-$link = mysql_connect($mysql_host, $mysql_user, $mysql_password)
+$link = mysql_connect($_SERVER["host"], $_SERVER["user"], $_SERVER["pass"])
     or die('No se pudo conectar');
 echo 'Connected successfully';
 mysql_select_db('my_database') or die('No se pudo seleccionar la base de datos');
