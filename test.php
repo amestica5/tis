8fa7b882937fe2c1679b15eb4d247b12ba3f689c
amestica5 <martin.amestica@gmail.com>
2015-11-11 18:55:50 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,8 @@
 <?php
-$link = mysql_connect($_SERVER["host"], $_SERVER["user"], $_SERVER["pass"])
-    or die('No se pudo conectar');
-echo 'Connected successfully';
-mysql_select_db('my_database') or die('No se pudo seleccionar la base de datos');
+$enlace =  mysql_connect($_SERVER["host"], $_SERVER["user"], $_SERVER["pass"]);
+if (!$enlace) {
+    die('No pudo conectarse: ' . mysql_error());
+}
+echo 'Conectado satisfactoriamente';
+mysql_close($enlace);
 ?>
