a10c71ac4d2f922cbd4ae959fd91cdf733867041
amestica5 <martin.amestica@gmail.com>
2015-11-12 19:22:32 -0300

    Update edit_table.php

--- edit_table.php
+++ edit_table.php
@@ -6,7 +6,7 @@ $basedatos = $_SERVER["base"];
 // Create connection
 $conn = new mysqli($servername, $username, $password,$basedatos);
 for ($i=1; $i<1500000;$i++){
-    echo "usuario".$i;
+    echo "usuario"." ".$i."<br>";
 $sql = "UPDATE usuarios SET nombre_usuario='usuario.$i' WHERE id=$i";
 }
 if ($conn->query($sql) === TRUE) {
