21fe8c2658616fddb6e328e74da9a3ed8dc44fbe
amestica5 <martin.amestica@gmail.com>
2015-11-12 19:38:41 -0300

    Update edit_table.php

--- edit_table.php
+++ edit_table.php
@@ -5,10 +5,10 @@ $password = $_SERVER["pass"];
 $basedatos = $_SERVER["base"];
 // Create connection
 $conn = new mysqli($servername, $username, $password,$basedatos);
-//for ($i=1; $i<11;$i++){
+for ($i=1; $i<11;$i++){
   //  echo "usuario"." ".$i."<br>";
-$sql = "UPDATE usuarios SET nombre_usuario=CONCAT(nombre_usuario,1) WHERE id=1";
-//}
+$sql = "UPDATE usuarios SET nombre_usuario=CONCAT(nombre_usuario,$i) WHERE id=$i";
+}
 if ($conn->query($sql) === TRUE) {
     echo "Record updated successfully";
 } else {
