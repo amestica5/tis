5c22e29ddc5e3a7b561a7d751e46b0b11f31726d
amestica5 <martin.amestica@gmail.com>
2015-11-12 16:26:42 -0300

    Update edit_table.php

--- edit_table.php
+++ edit_table.php
@@ -3,7 +3,14 @@ $servername = $_SERVER["host"];
 $username = $_SERVER["user"];
 $password = $_SERVER["pass"];
 $basedatos = $_SERVER["base"];
+// Create connection
+$conn = new mysqli($servername, $username, $password,$basedatos);
+$sql = "INSERT INTO usuarios (nombre_usuario)
+VALUES ('usuario1')";
 
-$enlace =  mysql_connect($servername, $username, $password);
-$resultado = mysql_query('INSERT INTO usuarios(nombre_usuario) VALUES("usuario1")');
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 ?>
