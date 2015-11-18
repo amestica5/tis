a233bbe19887c200364cbfcf0a2a900d393b24b2
amestica5 <martin.amestica@gmail.com>
2015-11-18 15:46:01 -0300

    Update search.php

--- search.php
+++ search.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
 } else{
     for($i=0; $i<50;$i++){
     $result = $conn->query("SELECT * FROM  `usuarios` WHERE id < $i"); 
-    while($obj = $result->fetch_object())
+    $obj = $result->fetch_object();
     echo $obj->nombre_usuario ."<br/>";
     }
 }
